9c7ada64d2b0d2ac69bfb90c3b37b72c2482b706
chrislonng <chris@lonng.org>
2017-02-04 16:55:15 +0800

    feature: proxy url.Values methods

--- form_helper.go
+++ form_helper.go
@@ -2,6 +2,8 @@ package nex
 
 import (
 	"strconv"
+	"bytes"
+	"sort"
 )
 
 func (f *uniform) Int(key string) int {
@@ -74,4 +76,36 @@ func (f *uniform) Uint64OrDefault(key string, def uint64) uint64 {
 		return def
 	}
 	return v
+}
+
+// Get gets the first value associated with the given key.
+// If there are no values associated with the key, Get returns
+// the empty string. To access multiple values, use the map
+// directly.
+func (f *uniform) Get(key string) string {
+	return f.Values.Get(key)
+}
+
+// Set sets the key to value. It replaces any existing
+// values.
+func (f *uniform) Set(key, value string) {
+	f.Values.Set(key,value)
+}
+
+// Add adds the value to key. It appends to any existing
+// values associated with key.
+func (f *uniform) Add(key, value string) {
+	f.Values.Add(key,value)
+}
+
+// Del deletes the values associated with key.
+func (f *uniform) Del(key string) {
+	f.Values.Del(key)
+}
+
+
+// Encode encodes the values into ``URL encoded'' form
+// ("bar=baz&foo=quux") sorted by key.
+func (f *uniform) Encode() string {
+	return f.Values.Encode()
 }
