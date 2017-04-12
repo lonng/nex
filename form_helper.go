4dd97456a29317233b102e6524aefb51722101c3
chrislonng <chris@lonng.org>
2017-04-12 13:01:58 +0800

    fixed: form default value

--- form_helper.go
+++ form_helper.go
@@ -19,7 +19,7 @@ func (f *uniform) Int(key string) int {
 func (f *uniform) IntOrDefault(key string, def int) int {
 	value := f.Get(key)
 	if value == "" {
-		return 0
+		return def
 	}
 	v, err := strconv.Atoi(value)
 	if err != nil {
@@ -43,7 +43,7 @@ func (f *uniform) Int64(key string) int64 {
 func (f *uniform) Int64OrDefault(key string, def int64) int64 {
 	value := f.Get(key)
 	if value == "" {
-		return 0
+		return def
 	}
 	v, err := strconv.ParseInt(value, 10, 64)
 	if err != nil {
@@ -67,7 +67,7 @@ func (f *uniform) Uint64(key string) uint64 {
 func (f *uniform) Uint64OrDefault(key string, def uint64) uint64 {
 	value := f.Get(key)
 	if value == "" {
-		return 0
+		return def
 	}
 	v, err := strconv.ParseUint(value, 10, 64)
 	if err != nil {
